5acf35da1daa01789d65d2a94464ec37d843cc2c
Rafael Gaino <rgaino@gmail.com>
2018-06-05 18:41:29 -0300

    filter out meditations with zero matches. Also fixed cloning bug where data was being passed as reference and causing the original data to be mutated.

--- models/Meditation.js
+++ models/Meditation.js
@@ -10,7 +10,8 @@ class Meditation {
   //based on input of emotions
   findByEmotions(emotions) {
     //Create a working copy of the data since it's hardcoded and we don't want to modify it on every request
-    var meditations = meditationData.meditations;
+    // var meditations = meditationData.meditations.slice();
+    var meditations = JSON.parse(JSON.stringify(meditationData.meditations));
 
     //loop through meditations and calculate how many emotions match
     meditations.forEach(meditation => {
@@ -20,10 +21,13 @@ class Meditation {
           matches++;
         }
       });
+
       //assign the number of matches to the meditation
       meditation.matches = matches;
     });
 
+    //filter out meditations with 0 matches
+    meditations = meditations.filter(meditation => meditation.matches > 0);
     //sort descending so the meditation with most matching emotions show up first
     meditations.sort((a, b) => b.matches - a.matches);
     return meditations;
