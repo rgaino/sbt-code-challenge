16a3fcafc14597c781a7ac392192898c9cb517c2
Rafael Gaino <rgaino@gmail.com>
2018-06-05 15:59:38 -0300

    Logging setup steps instead of comments

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const apiRoutes = require("./routes/apiRoutes");
 
-//Setting up API routes
 const app = express();
+console.log("Setting up API routes");
 apiRoutes(app);
 
 const PORT = process.env.PORT || 5000;
+console.log(`Setup completed, listening on ${PORT}`);
 app.listen(PORT);
