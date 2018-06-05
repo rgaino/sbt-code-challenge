b7b23d64d77e8ec49ca8782ff96dcc93a109d148
Rafael Gaino <rgaino@gmail.com>
2018-06-05 16:28:32 -0300

    Added comments for clarity

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,9 @@
-const Meditation = require("../models/Meditation");
+const Meditation = require("../models/Meditation"); //Meditation model
 
 module.exports = app => {
+  //this method receives a list of emotions and return associated meditations
   app.get("/api/meditations", (req, res) => {
     var meditation = new Meditation();
-
     res.send(meditation.findAll());
   });
 };
